52128831e7fbba6ea81fdca273fc8a83db734108
naga55 <e145741@ie.u-ryukyu.ac.jp>
2018-11-05 18:12:05 +0900

    twitterで認証する

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get 'home/message'
   get 'home/top'
   get 'home/main'
-  post "home/top" => "home#create"
+  # post "home/top" => "home#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#top"
+  devise_for :users, controllers: {
+      omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
 end
