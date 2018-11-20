2dec318194101b23e5c1ea3963fef8f3d6145589
HiroyukiTamae <e165759@ie.u-ryukyu.ac.jp>
2018-11-20 18:02:03 +0900

    Add Reply

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/top'
   get 'home/main'
   post "home/main" => "home#create"
+  post "home/message" => "home#reply"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#top"
   devise_for :users, controllers: {
