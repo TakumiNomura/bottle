1adf7a009c0bb687e45620c17082fef7c20172c4
TakumiNomura <e165764@ie.u-ryukyu.ac.jp>
2018-11-22 14:04:36 +0900

    add not_null to posts add default_value to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :receives
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :rememberable, :omniauthable, :registerable
